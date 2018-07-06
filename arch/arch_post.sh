ed6daa28760a012ecba08ffd7a0beef2b35ae620
Mattia Procopio <promat85@gmail.com>
2018-07-06 23:05:38 +0200

    Add jedi  & epc

--- arch/arch_post.sh
+++ arch/arch_post.sh
@@ -16,6 +16,7 @@ passwd matt
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable dhcpcd docker
+pip install jedi epc
 git clone https://github.com/MattBlack85/dotfiles.git /home/data/repos/dotfiles
 git clone https://github.com/robbyrussell/oh-my-zsh.git /home/matt/.oh-my-zsh
 git clone https://github.com/powerline/fonts.git /home/matt/data/repos/fonts
