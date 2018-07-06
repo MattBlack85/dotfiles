9de657ba32548f621d5b64c491ca6856186479bb
Mattia Procopio <promat85@gmail.com>
2018-07-06 13:08:18 +0200

    Fix path to raw file

--- arch/install.sh
+++ arch/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 pacstrap /mnt base
 genfstab -U /mnt >> /mnt/etc/fstab
-wget https://github.com/MattBlack85/dotfiles/blob/master/arch/arch_post.sh
+wget https://raw.githubusercontent.com/MattBlack85/dotfiles/master/arch/arch_post.sh
+chmod u+x arch_post.sh
 cp arch_post.sh /mnt/chroot.sh
 arch-chroot /mnt
 rm /mnt/chroot.sh
