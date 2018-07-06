819ab7af2d204a2589a09206181febf175b7dcba
Mattia Procopio <promat85@gmail.com>
2018-07-06 18:05:26 +0200

    Fix post bootstrap script

--- arch/arch_post.sh
+++ arch/arch_post.sh
@@ -9,7 +9,7 @@ cat <<EOF > /etc/hosts
 ::1		localhost
 127.0.1.1	achmatt.localdomain	archmatt
 EOF
-pacman -Sy sudo wpa_supplicant efibootmgr grub xmonad xmobar emacs firefox net-tools sbcl git xorg-server zsh pulseaudio gzip unzip openssh wget rxvt-unicode
+pacman -Sy sudo wpa_supplicant efibootmgr grub xmobar emacs firefox net-tools sbcl git xorg-server zsh pulseaudio gzip unzip openssh wget rxvt-unicode xorg-xinit xmonad xmonad-contrib
 passwd
 useradd -m matt
 passwd matt
@@ -18,13 +18,13 @@ grub-mkconfig -o /boot/grub/grub.cfg
 systemctl enable dhcpcd
 git clone https://github.com/MattBlack85/dotfiles.git /home/data/repos/dotfiles
 git clone https://github.com/robbyrussell/oh-my-zsh.git /home/matt/.oh-my-zsh
-git clone https://github.com/powerline/fonts.git /home/matt/repos/fonts
-export $HOME=/home/matt/ && ./home/matt/repos/fonts/install.sh
+git clone https://github.com/powerline/fonts.git /home/matt/data/repos/fonts
+export HOME=/home/matt/ && ./home/matt/data/repos/fonts/install.sh
 ln -s /home/data/repos/dotfiles/.emacs /home/matt/.emacs
 ln -s /home/data/repos/dotfiles/zsh/.zshrc /home/matt/.zshrc
 ln -s /home/data/repos/dotfiles/.xmobarrc /home/matt/.xmobarrc
 ln -s /home/data/repos/dotfiles/.xinitrc /home/matt/.xinitrc
 ln -s /home/data/repos/dotfiles/.Xdefaults /home/matt/.Xdefaults
 mkdir /home/matt/.xmonad
-ln -s /home/data/repos/dotfiles/.xmonad/.xmonad.hs /home/matt/.xmonad/.xmonad.hs
+ln -s /home/data/repos/dotfiles/.xmonad/xmonad.hs /home/matt/.xmonad/
 exit
