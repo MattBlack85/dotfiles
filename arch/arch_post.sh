3bf766d1433ddeb6b6d89e7b3009c795919c683c
Mattia Procopio <promat85@gmail.com>
2018-07-06 23:20:51 +0200

    Upadate post bootstrap script

--- arch/arch_post.sh
+++ arch/arch_post.sh
@@ -13,22 +13,52 @@ pacman -Sy sudo wpa_supplicant efibootmgr grub xmobar python3 python-pip emacs f
 passwd
 useradd -m matt
 passwd matt
+
+# mount /data under /home
+mount /dev/sdb1 /home/matt/data
+if [ ! -d "/home/matt/data/repos" ]; then
+    mkdir /home/matt/data/repos
+fi
+
+# Install the bootloader
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+
+# Enable services at startup
 systemctl enable dhcpcd docker
+
+# Install stuff for python
 pip install jedi epc
-git clone https://github.com/MattBlack85/dotfiles.git /home/data/repos/dotfiles
-git clone https://github.com/robbyrussell/oh-my-zsh.git /home/matt/.oh-my-zsh
-git clone https://github.com/powerline/fonts.git /home/matt/data/repos/fonts
+
+# Check if we have repos already available befoe checking them out
+if [ ! -d "/home/matt/data/repos/dotfiles" ]; then
+    git clone https://github.com/MattBlack85/dotfiles.git /home/matt/data/repos/dotfiles
+fi
+if [ ! -d "/home/matt/.oh-my-zsh" ]; then
+    git clone https://github.com/robbyrussell/oh-my-zsh.git /home/matt/.oh-my-zsh
+fi
+if [ ! -d "/home/matt/data/repos/fonts" ]; then
+    git clone https://github.com/powerline/fonts.git /home/matt/data/repos/fonts
+fi
+
+# Install fonts for powerline
 export HOME=/home/matt/ && ./home/matt/data/repos/fonts/install.sh
-ln -s /home/data/repos/dotfiles/.emacs /home/matt/.emacs
-ln -s /home/data/repos/dotfiles/zsh/.zshrc /home/matt/.zshrc
-ln -s /home/data/repos/dotfiles/.xmobarrc /home/matt/.xmobarrc
-ln -s /home/data/repos/dotfiles/.xinitrc /home/matt/.xinitrc
-ln -s /home/data/repos/dotfiles/.Xdefaults /home/matt/.Xdefaults
+
+# Link dotfile to locations where they are used for real so they will be synced with git repos
+ln -s /home/matt/data/repos/dotfiles/.emacs /home/matt/.emacs
+ln -s /home/matt/data/repos/dotfiles/zsh/.zshrc /home/matt/.zshrc
+ln -s /home/matt/data/repos/dotfiles/.xmobarrc /home/matt/.xmobarrc
+ln -s /home/matt/data/repos/dotfiles/.xinitrc /home/matt/.xinitrc
+ln -s /home/matt/data/repos/dotfiles/.Xdefaults /home/matt/.Xdefaults
 mkdir /home/matt/.xmonad
 ln -s /home/data/repos/dotfiles/.xmonad/xmonad.hs /home/matt/.xmonad/
+
+# folders, links and files have been manipulated all by root, change owner
 chown matt:matt /home/matt -R
+
+# VERY IMPORTANT for powerline fonts
 localectl set-locale en_US.utf8
+
+# Set the best shell for my user
 chsh -s /bin/zsh matt
 exit
