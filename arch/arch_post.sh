93e82fc94241c4667c8cadc86a8ccbf0ce18af61
Mattia Procopio <promat85@gmail.com>
2018-07-09 10:19:14 +0200

    Add missing dependencies for rambox

--- arch/arch_post.sh
+++ arch/arch_post.sh
@@ -9,7 +9,7 @@ cat <<EOF > /etc/hosts
 ::1		localhost
 127.0.1.1	archmatt.localdomain	archmatt
 EOF
-pacman -Sy sudo wpa_supplicant efibootmgr grub xmobar python3 python-pip emacs firefox net-tools sbcl git xorg-server zsh pulseaudio gzip unzip openssh wget rxvt-unicode xorg-xinit xmonad xmonad-contrib docker alsa-utils xrandr
+pacman -Sy sudo wpa_supplicant efibootmgr grub xmobar python3 python-pip emacs firefox net-tools sbcl git xorg-server zsh pulseaudio gzip unzip openssh wget rxvt-unicode xorg-xinit xmonad xmonad-contrib docker alsa-utils xrandr libxss gtk2
 passwd
 useradd -m matt
 passwd matt
