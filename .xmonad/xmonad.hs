c76f822559aec5db7a258faf9446ecb8a802f5fe
Mattia Procopio <promat85@gmail.com>
2021-11-09 15:06:43 +0100

    Rework xmonad using DT inputs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,25 +1,267 @@
 import XMonad
+import System.Directory
+import System.IO (hPutStrLn)
+import System.Exit (exitSuccess)
+import qualified XMonad.StackSet as W
+
+    -- Actions
+import XMonad.Actions.CopyWindow (kill1)
+import XMonad.Actions.CycleWS (Direction1D(..), moveTo, shiftTo, WSType(..), nextScreen, prevScreen)
+import XMonad.Actions.GridSelect
+import XMonad.Actions.MouseResize
+import XMonad.Actions.Promote
 import XMonad.Actions.SpawnOn
-import XMonad.Hooks.DynamicLog
+import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
+import XMonad.Actions.WindowGo (runOrRaise)
+import XMonad.Actions.WithAll (sinkAll, killAll)
+import qualified XMonad.Actions.Search as S
 
+    -- Data
+import Data.Char (isSpace, toUpper)
+import Data.Maybe (fromJust)
+import Data.Monoid
+import Data.Maybe (isJust)
+import Data.Tree
+import qualified Data.Map as M
 
-myWorkspaces = ["rambox", "web", "editor", "terms", "alsa", "chromium"] ++ ["7..9"]
+    -- Hooks
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
+import XMonad.Hooks.EwmhDesktops  -- for some fullscreen events, also for xcomposite in obs.
+import XMonad.Hooks.ManageDocks (avoidStruts, docksEventHook, manageDocks, ToggleStruts(..))
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat, doCenterFloat)
+import XMonad.Hooks.ServerMode
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.WorkspaceHistory
 
-main = xmonad =<< xmobar myConfig
+    -- Layouts
+import XMonad.Layout.Accordion
+import XMonad.Layout.GridVariants (Grid(Grid))
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.Spiral
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Tabbed
+import XMonad.Layout.ThreeColumns
 
-myConfig = def
-  { terminal           = "urxvt"
-    , modMask            = mod4Mask
-    , borderWidth        = 3
-    , workspaces         = myWorkspaces
-    , focusedBorderColor = "#00BFFF"
-    , normalBorderColor  = "#000000"
-    , manageHook = manageSpawn
-    , startupHook        = do
-        spawnOn "editor" "emacs"
-        spawnOn "web" "firefox"
-        spawnOn "terms" "urxvt -e weechat"
-        spawnOn "terms" "urxvt"
-        spawnOn "rambox" "rambox"
-	spawnOn "alsa" "urxvt -e alsamixer"
+    -- Layouts modifiers
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.Magnifier
+import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), (??))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Renamed
+import XMonad.Layout.ShowWName
+import XMonad.Layout.Simplest
+import XMonad.Layout.Spacing
+import XMonad.Layout.SubLayouts
+import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
+import XMonad.Layout.WindowNavigation
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+import qualified XMonad.Layout.MultiToggle as MT (Toggle(..))
+
+   -- Utilities
+import XMonad.Util.Dmenu
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.Run (runProcessWithInput, safeSpawn, spawnPipe)
+import XMonad.Util.SpawnOnce
+
+myFont :: String
+myFont = "xft:SauceCodePro Nerd Font Mono:regular:size=9:antialias=true:hinting=true"
+
+myShowWNameTheme :: SWNConfig
+myShowWNameTheme = def
+    { swn_font              = "xft:Ubuntu:bold:size=60"
+    , swn_fade              = 1.0
+    , swn_bgcolor           = "#1c1f24"
+    , swn_color             = "#ffffff"
     }
+
+
+
+mySpacing :: Integer -> l a -> XMonad.Layout.LayoutModifier.ModifiedLayout Spacing l a
+mySpacing i = spacingRaw False (Border i i i i) True (Border i i i i) True
+-- Defining a bunch of layouts, many that I don't use.
+-- limitWindows n sets maximum number of windows displayed for layout.
+-- mySpacing n sets the gap size around the windows.
+tall     = renamed [Replace "tall"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ limitWindows 12
+           $ mySpacing 8
+           $ ResizableTall 1 (3/100) (1/2) []
+magnify  = renamed [Replace "magnify"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ magnifier
+           $ limitWindows 12
+           $ mySpacing 8
+           $ ResizableTall 1 (3/100) (1/2) []
+monocle  = renamed [Replace "monocle"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ limitWindows 20 Full
+floats   = renamed [Replace "floats"]
+           $ smartBorders
+           $ limitWindows 20 simplestFloat
+grid     = renamed [Replace "grid"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ limitWindows 12
+           $ mySpacing 8
+           $ mkToggle (single MIRROR)
+           $ Grid (16/10)
+spirals  = renamed [Replace "spirals"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ mySpacing 8
+           $ spiral (6/7)
+threeCol = renamed [Replace "threeCol"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ limitWindows 7
+           $ ThreeCol 1 (3/100) (1/2)
+threeRow = renamed [Replace "threeRow"]
+           $ smartBorders
+           $ windowNavigation
+           $ addTabs shrinkText myTabTheme
+           $ subLayout [] (smartBorders Simplest)
+           $ limitWindows 7
+           -- Mirror takes a layout and rotates it by 90 degrees.
+           -- So we are applying Mirror to the ThreeCol layout.
+           $ Mirror
+           $ ThreeCol 1 (3/100) (1/2)
+tabs     = renamed [Replace "tabs"]
+           -- I cannot add spacing to this layout because it will
+           -- add spacing between window and tabs which looks bad.
+           $ tabbed shrinkText myTabTheme
+tallAccordion  = renamed [Replace "tallAccordion"]
+           $ Accordion
+wideAccordion  = renamed [Replace "wideAccordion"]
+           $ Mirror Accordion
+
+-- setting colors for tabs layout and tabs sublayout.
+myTabTheme = def { fontName            = myFont
+                 , activeColor         = "#46d9ff"
+                 , inactiveColor       = "#313846"
+                 , activeBorderColor   = "#46d9ff"
+                 , inactiveBorderColor = "#282c34"
+                 , activeTextColor     = "#282c34"
+                 , inactiveTextColor   = "#d0d0d0"
+                 }
+
+myModMask :: KeyMask
+myModMask = mod4Mask
+
+myTerminal :: String
+myTerminal = "urxvt"
+
+myEditor :: String
+myEditor = "emacsclient -c -a 'emacs' "
+
+myBorderWidth :: Dimension
+myBorderWidth = 3
+
+myColor :: String
+myColor = "#282c34"
+
+myFocusColor :: String
+myFocusColor = "#46d9ff"
+
+myStartupHook :: X ()
+myStartupHook = do
+  spawnOnce "/usr/bin/emacs --daemon &"
+  spawnOnce "firefox"
+  spawnOnce "feh --bg-fill ~/data/Downloads/bubble-hst-3412x3240.png"
+  spawnOnce "pavucontrol"
+  spawnOnce "slack"
+  spawnOnce myEditor
+  spawnOn   " terms " "urxvt"
+
+myKeys :: [(String, X ())]
+myKeys =
+  [ ("M-S-a", killAll)   -- Kill all windows on current workspace
+  , ("M-.", nextScreen)  -- Switch focus to next monitor
+  , ("M-,", prevScreen)  -- Switch focus to prev monitor
+  , ("M-C-r", spawn "xmonad --recompile")       -- Recompiles xmonad
+  , ("M-S-r", spawn "xmonad --restart")         -- Restarts xmonad
+  , ("M-S-q", io exitSuccess)         
+  ]
+
+myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts floats
+  $ mkToggle (NBFULL ?? NOBORDERS ?? EOT) myDefaultLayout
+  where
+    myDefaultLayout =     withBorder myBorderWidth tall
+                          ||| magnify
+                          ||| noBorders monocle
+                          ||| floats
+                          ||| noBorders tabs
+                          ||| grid
+                          ||| spirals
+                          ||| threeCol
+                          ||| threeRow
+                          ||| tallAccordion
+                          ||| wideAccordion
+
+myManageHook :: XMonad.Query (Data.Monoid.Endo WindowSet)
+myManageHook = composeAll
+     -- 'doFloat' forces a window to float.  Useful for dialog boxes and such.
+     -- using 'doShift ( myWorkspaces !! 7)' sends program to workspace 8!
+     -- I'm doing it this way because otherwise I would have to write out the full
+     -- name of my workspaces and the names would be very long if using clickable workspaces.
+     [ className =? "confirm"         --> doFloat
+     , className =? "file_progress"   --> doFloat
+     , className =? "dialog"          --> doFloat
+     , className =? "download"        --> doFloat
+     , className =? "error"           --> doFloat
+     , className =? "Gimp"            --> doFloat
+     , className =? "notification"    --> doFloat
+     , className =? "toolbar"         --> doFloat
+     , title =? "Mozilla Firefox"     --> doShift " web "
+     , className =? "Emacs"           --> doShift " emacs "
+     , className =? "Slack"           --> doShift " chat "
+     , className =? "Pavucontrol"     --> doShift " sound "
+     , (className =? "Firefox" <&&> resource =? "Dialog") --> doFloat  -- Float Firefox Dialog
+     ]
+
+clickable ws = "<action=xdotool key super+"++show i++">"++ws++"</action>"
+    where i = fromJust $ M.lookup ws myWorkspaceIndices
+
+myWorkspaces = [" image ", " web ", " emacs ", " terms ", " video ", " chat ", " mix ", " sound "]
+myWorkspaceIndices = M.fromList $ zipWith (,) myWorkspaces [1..] -- (,) == \x y -> (x,y)
+
+main :: IO()
+
+main = do
+  xmproc0 <- spawnPipe "xmobar -x 0 $HOME/.xmobarrc"
+  xmproc1 <- spawnPipe "xmobar -x 1 $HOME/.xmobarrc"
+
+  xmonad $ ewmh def
+    { terminal           = myTerminal
+    , modMask            = myModMask
+    , workspaces         = myWorkspaces
+    , manageHook         = manageDocks <+> myManageHook
+    , layoutHook         = showWName' myShowWNameTheme $ myLayoutHook
+    , handleEventHook    = docksEventHook
+    , borderWidth        = myBorderWidth
+    , focusedBorderColor = myFocusColor
+    , normalBorderColor  = myColor
+    , startupHook        = myStartupHook
+    , logHook = dynamicLogWithPP $ xmobarPP
+      -- the following variables beginning with 'pp' are settings for xmobar.
+      { ppOutput = \x -> hPutStrLn xmproc0 x                          -- xmobar on monitor 1
+                         >> hPutStrLn xmproc1 x                          -- xmobar on monitor 2
+      }
+    } `additionalKeysP` myKeys
